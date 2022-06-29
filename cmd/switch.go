859509b459c39dc27a8b4d349231c22a612ebc65
DevelopNaoki <55051212+DevelopNaoki@users.noreply.github.com>
2022-06-29 13:20:08 +0900

    Update switch.go

--- cmd/switch.go
+++ cmd/switch.go
@@ -108,3 +108,37 @@ var switchRename = &cobra.Command{
 		}
 	},
 }
+
+var switchOptionCfgCmd = &cobra.Command{
+	Use:   "configure",
+	Short: "Configure switch option",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return fmt.Errorf("need valid command")
+	},
+}
+
+var switchType string
+var switchChangeOptionCfgType = &cobra.Command{
+	Use:   "type",
+	Short: "Configure switch type",
+	Args:  cobra.RangeArgs(1, 1),
+	Run: func(cmd *cobra.Command, args []string) {
+		err := process.ChangeSwitchType(args[0], switchType)
+		if err != nil {
+			fmt.Print(err)
+		}
+	},
+}
+
+var netAdapter string
+var switchOptionCfgNetAdapter = &cobra.Command{
+	Use:   "adapter",
+	Short: "Configure network adapter",
+	Args:  cobra.RangeArgs(1, 1),
+	Run: func(cmd *cobra.Command, args []string) {
+		err := process.ChangeSwitchNetAdapter(args[0], netAdapter)
+		if err != nil {
+			fmt.Print(err)
+		}
+	},
+}
