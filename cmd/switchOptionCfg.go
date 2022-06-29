27896088a1c2ea4df1c4a40f1b06a6514b0bf404
DevelopNaoki <55051212+DevelopNaoki@users.noreply.github.com>
2022-06-29 10:13:45 +0900

    Update and rename switchChange.go to switchOptionCfg.go

--- cmd/switchOptionCfg.go
+++ cmd/switchOptionCfg.go
@@ -7,18 +7,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var switchChangeCmd = &cobra.Command{
-	Use:   "change",
-	Short: "Change switch option",
+var switchOptionCfgCmd = &cobra.Command{
+	Use:   "configure",
+	Short: "Configure switch option",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("need valid command")
 	},
 }
 
 var switchType string
-var switchChangeType = &cobra.Command{
+var switchChangeOptionCfgType = &cobra.Command{
 	Use:   "type",
-	Short: "Change switch type",
+	Short: "Configure switch type",
 	Args:  cobra.RangeArgs(1, 1),
 	Run: func(cmd *cobra.Command, args []string) {
 		err := process.ChangeSwitchType(args[0], switchType)
@@ -29,9 +29,9 @@ var switchChangeType = &cobra.Command{
 }
 
 var netAdapter string
-var switchChangeNetAdapter = &cobra.Command{
+var switchOptionCfgNetAdapter = &cobra.Command{
 	Use:   "adapter",
-	Short: "Change network adapter",
+	Short: "Configure network adapter",
 	Args:  cobra.RangeArgs(1, 1),
 	Run: func(cmd *cobra.Command, args []string) {
 		err := process.ChangeSwitchNetAdapter(args[0], netAdapter)
