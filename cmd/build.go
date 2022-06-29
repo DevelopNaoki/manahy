50efa61041c8c50998577e2afb50e1a7a86b2283
DevelopNaoki <55051212+DevelopNaoki@users.noreply.github.com>
2022-06-29 11:09:07 +0900

    Update build.go

--- cmd/build.go
+++ cmd/build.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/DevelopNaoki/manahy/process"
+	"github.com/DevelopNaoki/manahy/modules"
 	"github.com/spf13/cobra"
 )
 
@@ -11,20 +11,20 @@ var build = &cobra.Command{
 	Use:   "build",
 	Short: "create vm, disk and switch from manahy.yaml",
 	Run: func(cmd *cobra.Command, args []string) {
-		data, err := process.UnmarshalYaml("manahy.yaml")
+		data, err := modules.UnmarshalYaml("manahy.yaml")
 		if err != nil {
 			fmt.Printf("%w\n", err)
 		}
 
 		for i := range data.Disks {
-			err = process.CreateDisk(data.Disks[i])
+			err = modules.CreateDisk(data.Disks[i])
 			if err != nil {
 				fmt.Printf("%w\n", err)
 			}
 		}
 
 		for i := range data.Networks {
-			err = process.CreateSwitch(data.Networks[i])
+			err = modules.CreateSwitch(data.Networks[i])
 			if err != nil {
 				fmt.Printf("%w\n", err)
 			}
