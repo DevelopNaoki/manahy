805655d0c9153113ff8b5c8b1a330b8a219ce8a6
DevelopNaoki <55051212+DevelopNaoki@users.noreply.github.com>
2022-07-20 18:02:36 +0900

    Update build.go

--- modules/build.go
+++ modules/build.go
@@ -5,6 +5,16 @@ inport (
 )
 
 func BuildByStruct (summarize Summarize){
- 
+  for _, disk := range summarize.Disks {
+    CreateDisk(disk)
+  }
+  
+  for _, network := range summarize.Networks {
+    CreateSwitch(network)
+  }
+  
+  for _, vm := range summarize.Vms {
+    CreateVm(vm)
+  }
 }
 
