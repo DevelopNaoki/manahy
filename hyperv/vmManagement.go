39b06c43ca098deb469376fda8cf9c0734757b88
DevelopNaoki <develop.naoki@gmail.com>
2022-09-24 11:55:17 +0900

    Delete unit conversion functions

--- hyperv/vmManagement.go
+++ hyperv/vmManagement.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os/exec"
 	"regexp"
-	"strconv"
 	"strings"
 
 	"github.com/DevelopNaoki/manahy/internal"
@@ -128,21 +127,3 @@ func RemoveVmById(vmid string) error {
 	return nil
 }
 
-func unitConversion(num string) string {
-	f, _ := strconv.ParseFloat(num, 64)
-	unit := "B"
-	for f >= 1024 {
-		f = f / 1024
-		switch unit {
-		case "B":
-			unit = "KB"
-		case "KB":
-			unit = "MB"
-		case "MB":
-			unit = "GB"
-		case "GB":
-			unit = "TB"
-		}
-	}
-	return strconv.FormatFloat(f, 'f', 0, 64) + unit
-}
