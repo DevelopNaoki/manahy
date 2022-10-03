1ff28505585a44b7d9284f2c8dcd0af03d632ae2
DevelopNaoki <55051212+DevelopNaoki@users.noreply.github.com>
2022-10-03 16:26:29 +0900

    Update unitConversion.go

--- internal/unitConversion.go
+++ internal/unitConversion.go
@@ -7,8 +7,8 @@ import (
 func ConversionBtoXB(num string) string {
         f, _ := strconv.ParseFloat(num, 64)
         unit := "B"
-        for f >= 1024 {
-                f = f / 1024
+        for f >= 1000 {
+                f = f / 1000
                 switch unit {
                 case "B":
                         unit = "KB"
@@ -21,4 +21,25 @@ func ConversionBtoXB(num string) string {
                 }
         }
         return strconv.FormatFloat(f, 'f', 0, 64) + unit
+}
+
+func ConversionXBtoB(value string) string {
+	num := regexp.MustCompile("[0-9.]*").Split(value, -1)
+	unit := regexp.MustCompile("[KMGTP]B").Split(value, -1)
+        f, _ := strconv.ParseFloat(num, 64)
+	
+        for unit == "B" {
+                f = f * 1000
+                switch unit {
+                case "TB":
+                        unit = "GB"
+                case "GB":
+                        unit = "MB"
+                case "MB":
+                        unit = "KB"
+		case "KB":
+			unit = "B"
+                }
+        }
+        return strconv.FormatFloat(f, 'f', 0, 64)
 }
