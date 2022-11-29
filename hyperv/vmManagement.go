ac17eb632312be15b7f8a433b76b8504b6e57dd3
DevelopNaoki <55051212+DevelopNaoki@users.noreply.github.com>
2022-11-30 00:02:18 +0900

    Update vmManagement.go

--- hyperv/vmManagement.go
+++ hyperv/vmManagement.go
@@ -21,7 +21,7 @@ func GetVmList() (vmList []Vm, err error) {
 		var vm Vm
 		split := regexp.MustCompile("\r\n|\n").Split(vms[i], -1)
 		for j := range split {
-			switch {
+			switch true {
 			case strings.Contains(split[j], "VMName"):
 				vmName := regexp.MustCompile(":").Split(split[j], -1)
 				vm.VmName = strings.TrimSpace(vmName[1])
@@ -38,7 +38,6 @@ func GetVmList() (vmList []Vm, err error) {
 				memory := regexp.MustCompile(":").Split(split[j], -1)
 				vm.Memory = internal.ConversionBtoXB(strings.TrimSpace(memory[1]))
 			default:
-				fmt.Print("aaaaaaaaaaaaaaaaaaa")
 			}
 		}
 		vmList = append(vmList, vm)
