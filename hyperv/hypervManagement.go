eb35195b37b4e47f3b9e60dc47dcbe265164ccc0
DevelopNaoki <55051212+DevelopNaoki@users.noreply.github.com>
2022-11-29 09:29:47 +0900

    Update hypervManagement.go

--- hyperv/hypervManagement.go
+++ hyperv/hypervManagement.go
@@ -7,17 +7,6 @@ import (
 	"strings"
 )
 
-func IsAdmin() (isAdmin bool, err error) {
-	res, err := exec.Command("powershell", "-NoProfile", "(New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)").Output()
-	if err != nil {
-		return false, fmt.Errorf("Failed to execute the command to check if the user has administrative privileges.")
-	}
-	if strings.TrimSpace(string(res)) == "True" {
-		return true, nil
-	}
-	return false, nil
-}
-
 func CheckHypervEnabled() (enabled bool, err error) {
 	res, err := exec.Command("powershell", "-NoProfile", "(Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V).State").Output()
 	if err != nil {
@@ -30,33 +19,17 @@ func CheckHypervEnabled() (enabled bool, err error) {
 }
 
 func EnableHyperv() error {
-	isAdmin, err := IsAdmin()
+        hasEnabled, err := CheckHypervEnabled()
 	if err != nil {
 		return err
 	}
-	if isAdmin {
-		enabled, err := CheckHypervEnabled()
-		if err != nil {
-			return err
-		}
-		if enabled {
-			return fmt.Errorf("Hyper-V is already enabled")
-		}
-	} else {
-		return fmt.Errorf("You do not have execute permission")
+	if hasEnabled {
+		return fmt.Errorf("Hyper-V is already enabled")
 	}
 
 	err = exec.Command("powershell", "-NoProfile", "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All").Run()
 	if err != nil {
 		return fmt.Errorf("Failed to execute command to enable Hyper-V")
-	}
-
-	enabled, err := CheckHypervEnabled()
-	if err != nil {
-		return err
-	}
-	if !enabled {
-		return fmt.Errorf("Failed to enable Hyper-V")
 	}
 	return nil
 }
